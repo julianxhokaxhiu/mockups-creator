7dba8b44bbd7509a47c5dfd8bae76ff3199ecece
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2014-08-31 14:22:20 +0200

    Do not remove folders but only items.

--- Gruntfile.js
+++ Gruntfile.js
@@ -146,9 +146,6 @@ module.exports = function(grunt) {
             grunt.initConfig({
                 pkg: grunt.file.readJSON('package.json'),
                 clean: {
-                    options: {
-                        force: true
-                    },
                     www: [
                         app.wwwPath,
                     ],
