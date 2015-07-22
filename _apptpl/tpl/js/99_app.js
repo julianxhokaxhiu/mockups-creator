3658a74e474a8a5a811feb835ef829f12ec8a462
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2015-07-22 09:24:03 +0200

    Provide a good starting point for new apps.
    _settings.scss: provide a flat user experience without smoothed angles.
    _overrides.scss: this is where all the framework override happens, so we don't need to modify bootstrap directly.
    _colors.scss: this is where all the colors of the app will live.
    screen.scss: include the new _colors.scss, _overrides.scss, Open Sans webfont and remove the empty body declaration. All the "main" styles will live in override. Screen will serve only as "widget logic" components styling.
    99_app.js: a starting point to write your Javascript for your app. Other libraries should start with lower number ( 01..98 ) so our script will always be the last one. Viewports are also already managed via "bootstrap-toolkit".

---
+++ _apptpl/tpl/js/99_app.js
@@ -0,0 +1,9 @@
+(function($, viewport){
+  $(function(){
+    var $window = $(window),
+        $html = $('html'),
+        $body = $('body'),
+
+    // Your code here
+  });
+})(jQuery, ResponsiveBootstrapToolkit);
