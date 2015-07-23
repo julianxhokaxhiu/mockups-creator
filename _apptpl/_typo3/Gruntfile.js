5b6c13c9fa9b8d75d622d67e240072f849d2a2a5
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2015-07-23 17:16:56 +0200

    New version of SASS requires this option.
    Enable path loading starting from where Gruntfile.js is.

--- _apptpl/_typo3/Gruntfile.js
+++ _apptpl/_typo3/Gruntfile.js
@@ -51,7 +51,8 @@ module.exports = function(grunt) {
       },
   		sass: {
         options: {
-          style: 'compressed'
+          style: 'compressed',
+          loadPath: '.'
         },
   			build: {
   				files: {
