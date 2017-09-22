7378596b0c205ed11677804e3d62890098cc9bf5
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-09-22 17:07:48 +0200

    Improve the run script

--- _apptpl/run.sh
+++ _apptpl/run.sh
@@ -3,8 +3,17 @@
 # Run the docker in development mode
 docker run \
     -d \
+    --name="mockups-creator" \
     --rm \
     -e "PRODUCTION=false" \
+    --expose=35729 \
     -p 8080:8080 \
+    -p 35729:35729 \
     -v "$(pwd):/app" \
-    julianxhokaxhiu/mockups-creator
+    julianxhokaxhiu/mockups-creator &>/dev/null
+
+# Follow logs
+docker logs \
+    -f \
+    mockups-creator
+    
