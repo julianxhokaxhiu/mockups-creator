c5b2c834fc0313d82b10aea0fb36901c3a5806c9
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-09-22 17:19:02 +0200

    Cleanup after stopping logs

--- _apptpl/run.sh
+++ _apptpl/run.sh
@@ -2,18 +2,21 @@
 
 # Run the docker in development mode
 docker run \
-    -d \
-    --name="mockups-creator" \
-    --rm \
-    -e "PRODUCTION=false" \
-    --expose=35729 \
-    -p 8080:8080 \
-    -p 35729:35729 \
-    -v "$(pwd):/app" \
-    julianxhokaxhiu/mockups-creator &>/dev/null
+  -d \
+  --name="mockups-creator" \
+  --rm \
+  -e "PRODUCTION=false" \
+  --expose=35729 \
+  -p 8080:8080 \
+  -p 35729:35729 \
+  -v "$(pwd):/app" \
+  julianxhokaxhiu/mockups-creator &>/dev/null
 
 # Follow logs
 docker logs \
-    -f \
-    mockups-creator
-    
+  -f \
+  mockups-creator
+
+# Stop the docker to start the cleanup
+docker stop \
+  mockups-creator &>/dev/null
